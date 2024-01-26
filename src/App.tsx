853c066f5afc5201dc8237d6a115c1ea22632215
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-01-26 00:53:18 -0300

    git action test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ export function App() {
   return (
     <>
      <HomePage/>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ea quisquam esse ab commodi optio ipsum officiis qui molestiae sapiente, quasi fugit excepturi beatae eos in voluptatem quae sint t</p>
     </>
   )
 }
