d4950111d8e2e168d98c50a8696a1c1f59e24c07
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-01-26 17:47:58 -0300

    teste

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -7,6 +7,7 @@ export const ProductPage = () => {
         <DefaultTemplate>
             <ProductSection/>
             <ProductList/>
+            <div style={{"background" : "black"}}> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores aliquid nihil quae. Modi perspiciatis inventore numquam repellendus, ex distinctio consequuntur molestias quisquam, quidem ut veritatis officiis molestiae repellat quos corrupti.</p></div>
         </DefaultTemplate>
     )
 }
