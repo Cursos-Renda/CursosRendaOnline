cb3bc13f52ac0331c021f85a986bea103cf97a89
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-01-31 22:44:31 -0300

    test logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,6 @@
+import logo from "../../assets/Logotipo_para_loja_de_informatica_e_computacao.png"
+import styles from "./styles.module.scss"
+
 export const Header = () => {
     return (
         <header className={styles.header}>
