a420845301be1afae41e8b4da4d224fe16dd5b21
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-02-13 12:48:59 -0300

    feat: changed logo image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
-import logo from "../../assets/Logotipo_para_loja_de_informatica_e_computacao.png"
-import styles from "./styles.module.scss"
+import logo from "../../assets/logo.svg";
+import styles from "./styles.module.scss";
 
 export const Header = () => {
-    return (
-        <header className={styles.header}>
-            <figure>
-                <img src={logo} alt="Logo" />
-            </figure>
-            <nav>
-                <li>Sobre</li>
-                <li>Produtos</li>
-                <li>Contatos</li>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <figure>
+        <img src={logo} alt="Logo" />
+      </figure>
+      <nav>
+        <li>Sobre</li>
+        <li>Produtos</li>
+        <li>Contatos</li>
+      </nav>
+    </header>
+  );
+};
