97a20fa0a5eab8f3861037a724b053b2eabdb98b
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-03-20 19:03:04 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BannerSection } from "./components/sections/bannerSections";
 import { ContactSection } from "./components/sections/contactSection";
 import { Roulette } from "./components/sections/rouletteSection";
+import { DefaultTemplate } from "./template/defaultTemplate";
 
 export function App() {
 
   return (
     <>
+    <DefaultTemplate>
       <BannerSection />
       <Roulette />
       <ContactSection/>
+    </DefaultTemplate>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import "./style/index.scss"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import { DefaultTemplate } from "./template/defaultTemplate/index.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DefaultTemplate>
     <App />
-    </DefaultTemplate>
   </React.StrictMode>,
 )
