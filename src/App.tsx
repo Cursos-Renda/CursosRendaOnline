b710302460422851c847536268585b68a97bf6ed
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-03-20 19:07:04 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BannerSection } from "./components/sections/bannerSections/BannerSection";
-import { ContactSection } from "./components/sections/contactSection";
-import { Roulette } from "./components/sections/rouletteSection";
-import { DefaultTemplate } from "./template/defaultTemplate";
+import { BannerSection } from "./components/sections/bannerSections/BannerSection.tsx";
+import { ContactSection } from "./components/sections/contactSection/index.tsx";
+import { Roulette } from "./components/sections/rouletteSection/index.tsx";
+import { DefaultTemplate } from "./template/defaultTemplate/index.tsx";
 
 export function App() {
 
