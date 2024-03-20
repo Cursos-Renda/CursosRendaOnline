df00805f9e4098f4d4739a46508ce9b3fddffdbd
andreycaetano <133555380+andreycaetano@users.noreply.github.com>
2024-03-20 19:04:21 -0300

    deploy

--- src/template/defaultTemplate/index.tsx
+++ src/template/defaultTemplate/index.tsx
@@ -1,11 +1,7 @@
 import { Footer } from "../../components/footer"
 import { Header } from "../../components/header"
 
-interface DefaultTemplateProps {
-    children: React.ReactNode;
-}
-
-export const DefaultTemplate: React.FC<DefaultTemplateProps> = ({ children }) => {
+export const DefaultTemplate = ({ children }: any) => {
 
 
     return (
